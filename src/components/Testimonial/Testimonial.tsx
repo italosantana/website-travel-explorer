354a9d6b5036d43e5f831715277abe8addea53cf
Ítalo <italosanttana@hotmail.com>
2023-02-13 14:23:29 -0300

    chore: add stars rating component

--- src/components/Testimonial/Testimonial.tsx
+++ src/components/Testimonial/Testimonial.tsx
@@ -1,6 +1,30 @@
 import { Box, Container, Typography } from "@mui/material"
 
 
+const StarRating = () => {
+    return (
+        <Box>
+            <img src='../../src/assets/Star.png' alt="stars quantity rating by user" />
+            <img src='../../src/assets/Star.png' alt="stars quantity rating by user" />
+            <img src='../../src/assets/Star.png' alt="stars quantity rating by user" />
+            <img src='../../src/assets/Star.png' alt="stars quantity rating by user" />
+            <img src='../../src/assets/Star.png' alt="stars quantity rating by user" />
+
+            <Typography sx={{  fontFamily: "Inter",
+                    fontSize: '20px',
+                    fontWeight: 500,
+                    color: "#2D3134",
+                    }}>Andrew Sarma</Typography>
+
+            <Typography sx={{  fontFamily: "Inter",
+                    fontSize: '12px',
+                    fontWeight: 400,
+                    color: "#5B5F62",
+                    }}>Enterpreneur</Typography>
+        </Box>
+    )
+}
+
 const CardUserTestimonial = () => {
     return (
         <Box
@@ -14,6 +38,7 @@ const CardUserTestimonial = () => {
                 display: 'flex',
                 justifyContent: 'center',
                 flexDirection: 'column',
+                p: '30px',
             }}
         >
             <Typography
@@ -23,10 +48,12 @@ const CardUserTestimonial = () => {
                     fontWeight: 400,
                     color: "#5B5F62",
                     alignItems: 'center',
+                    mb: '27px',
                 }}
             >
                 Salty helped me a lot in finding the best place for our first outdoor adventure trip. They responded very quickly and gave me a detailed account of the place—its history, as well as its best features.
             </Typography>
+            <StarRating />
         </Box>
     )
 }
