eccccc1845ba37c3fbc59ee3732c4d4b3d12cbeb
Ítalo Santana <techitalosantana@gmail.com>
2023-02-23 09:56:32 -0300

    feat: add responsive testimonial

--- src/components/Testimonial/Testimonial.tsx
+++ src/components/Testimonial/Testimonial.tsx
@@ -1,4 +1,5 @@
 import { Box, Container, Typography } from "@mui/material"
+import theme from "../../styles/theme"
 
 
 const StarRating = () => {
@@ -10,17 +11,19 @@ const StarRating = () => {
             <img src='../../src/assets/Star.png' alt="stars quantity rating by user" />
             <img src='../../src/assets/Star.png' alt="stars quantity rating by user" />
 
-            <Typography sx={{  fontFamily: "Inter",
-                    fontSize: '20px',
-                    fontWeight: 500,
-                    color: "#2D3134",
-                    }}>Andrew Sarma</Typography>
+            <Typography sx={{
+                fontFamily: "Inter",
+                fontSize: '20px',
+                fontWeight: 500,
+                color: "#2D3134",
+            }}>Andrew Sarma</Typography>
 
-            <Typography sx={{  fontFamily: "Inter",
-                    fontSize: '12px',
-                    fontWeight: 400,
-                    color: "#5B5F62",
-                    }}>Enterpreneur</Typography>
+            <Typography sx={{
+                fontFamily: "Inter",
+                fontSize: '12px',
+                fontWeight: 400,
+                color: "#5B5F62",
+            }}>Enterpreneur</Typography>
         </Box>
     )
 }
@@ -34,11 +37,14 @@ const CardUserTestimonial = () => {
                 backgroundColor: "#FFF",
                 width: '537px',
                 height: '283px',
-                maxWidth: "100%",
+                maxWidth: "90%",
                 display: 'flex',
                 justifyContent: 'center',
                 flexDirection: 'column',
                 p: '30px',
+                [theme.breakpoints.down('mobile')]: {
+                    height: '100%',
+                }
             }}
         >
             <Typography
@@ -58,7 +64,7 @@ const CardUserTestimonial = () => {
     )
 }
 
-export const Termonial = () => {
+export const Testimonial = () => {
     return (
         <Container>
             <Box
@@ -68,8 +74,11 @@ export const Termonial = () => {
                     height: '671px',
                     width: "1440px",
                     maxWidth: "100%",
-                    flexWrap: "wrap",
                     justifyContent: 'space-between',
+                    [theme.breakpoints.down('tablet')]:{
+                        height: "100%",
+                        m: "20px 0px 20px 0px",
+                    }
                 }}
             >
 
@@ -82,7 +91,10 @@ export const Termonial = () => {
                         backgroundColor: '#EE8162',
                         img: {
                             borderRadius: '245px',
-                        }
+                        },
+                        [theme.breakpoints.down('desktop')]: {
+                            display: 'none',
+                        },
                     }}
                 >
                     <img src="../../../src/assets/client.png" alt="showing a client take a picture" />
